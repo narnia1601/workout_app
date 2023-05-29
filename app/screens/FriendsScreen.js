d4e38bca7864c1608c6ab53c594e347732c41d5c
Aloysius Tan <aloys-ta@COE001.local>
2023-05-29 11:34:21 +0800

    added friend component to FriendsScreen

--- app/screens/FriendsScreen.js
+++ app/screens/FriendsScreen.js
@@ -1,16 +1,37 @@
-import { Text, StyleSheet } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Friend from "../components/Friend";
 
-export default function FriendsScreen(){
+export default function FriendsScreen({ navigation }){
+    const navigateToHomeScreen = () => {
+        navigation.navigate('Tabs')
+    }
+
     return (
         <SafeAreaView>
-            <Text style={style.text}>Friends Page</Text>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={navigateToHomeScreen}>
+                    <Image source={require('../assets/arrow.png')} style={styles.image} tintColor="white"></Image>
+                </TouchableOpacity>
+            </View>
+            <Friend></Friend>
         </SafeAreaView>
     )
 }
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     text: {
         color: 'white'
+    },
+    container: {
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 15,
+        marginBottom: 20
+    },
+    image: {
+        width: 25,
+        height: 25
     }
 })
