6e4d0f2c37350588068f24e3d5b11e50cb6b3c10
Aloysius Tan <aloys-ta@COE001.local>
2023-06-05 14:30:44 +0800

    edited backend code

--- backend/main.py
+++ backend/main.py
@@ -4,33 +4,35 @@ app = Flask(__name__)
 
 @app.route('/register', methods=['POST'])
 def registerUser():
-    # store user's name and phone number
-    return
+    # store user's name and phone number, json returns name of user and phone number
+    data = request.get_json()
+    name = data.get('name')
+    phone_number = data.get('phone_number')
+    return 'User registration successful'
 
-@app.route('/user/{userId}', methods=['GET'])
-def getUser(userId):
-    # get user
-    return
-
-@app.route('/workouts/{userId}', methods=['GET'])
+@app.route('/workouts/<int:userId>', methods=['GET'])
 def getWorkouts(userId):
     # get workout history
     return
 
-@app.route('/friends/{userId}', methods=['GET'])
+@app.route('/friends/<int:userId>', methods=['GET'])
 def getFriends(userId):
     # get friends
     return
 
 @app.route('/addFriend', methods=['POST'])
 def addFriend():
-    # add friend
-    return
+    # add friend by friend's userId
+    data = request.get_json()
+    friendId = data.get('friendId')
+    userId = data.get('userId')
+    return 'Friend added successfully'
 
 @app.route('/addWorkout', methods=['POST'])
 def addWorkout():
     # add new workout
-    return
+    data = request.get_json()
+    return 'Workout added successfully'
 
 if __name__ == '__main__':
     app.run()
